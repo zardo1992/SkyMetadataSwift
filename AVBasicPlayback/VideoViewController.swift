cddf4d7a684c2d2e4419076380cf5c83fb7ef82b
Matteo Zardoni <matteo@MacBook-Pro-di-Matteo-Zardoni-Live-Reply.local>
2017-12-18 16:51:15 +0100

    Ultima versione funzionante

---
+++ AVBasicPlayback/VideoViewController.swift
@@ -0,0 +1,138 @@
+//
+//  VideoViewController.swift
+//  AVBasicPlayback
+//
+//  Created by Matteo Zardoni on 18/12/17.
+//  Copyright © 2017 Antonio Di Raffaele. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+import Foundation
+
+class VideoViewController: UIViewController {
+    
+    //@Objects
+    //=====================================================
+    var player: AVPlayer!
+    var playerLayer: AVPlayerLayer!
+    var playerItem: AVPlayerItem!
+    
+    //@IBOutlets
+    //=====================================================
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var videoView: UIView!
+    @IBOutlet weak var playPauseViewButton: UIView!
+    
+    //@IBActions
+    //=====================================================
+    @IBAction func eraseTextView(_ sender: Any) {
+        textView.text = ""
+    }
+    
+    @IBAction func copyToClipboard(_ sender: Any) {
+        UIPasteboard.general.string = textView.text
+    }
+    
+    @IBAction func goBack(_ sender: Any) {
+        textView.text = ""
+        player.pause()
+        player = nil
+        self.dismiss(animated: true, completion: nil)
+        self.performSegue(withIdentifier: "returnToView", sender: self)
+    }
+    
+    
+    //@Override functions
+    //=====================================================
+    override func observeValue(forKeyPath: String?, of: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        
+        if forKeyPath != "timedMetadata" {
+            return
+        }
+        
+        let data: AVPlayerItem
+            
+        data = of as! AVPlayerItem
+        
+        for item in data.timedMetadata! {
+            
+            var lastString: String
+                
+            lastString = textView.text
+            if lastString == "NO META DATA RECEIVED" {
+                textView.text = ""
+            }
+            
+            if item.stringValue != nil {
+                lastString = textView.text + " " + item.stringValue!
+                textView.text = lastString
+                
+                if let dataText = textView.text.data(using: .utf8) {
+                    let parser = XMLParser(data: dataText)
+                    let success : Bool = parser.parse()
+                    
+                    if success {
+                        textView.layer.borderWidth = 1
+                        textView.layer.borderColor = UIColor.green.cgColor
+                    } else {
+                        textView.layer.borderWidth = 1
+                        textView.layer.borderColor = UIColor.red.cgColor
+                    }
+                }
+            }
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        textView.layer.borderWidth = 1
+        textView.layer.borderColor = UIColor.black.cgColor
+        
+        if let value = Shared.shared.stringValue {
+            
+            guard let contentURL = URL(string: value) else {
+                let alert = UIAlertController(title: "A problem occurred!", message: "ERROR! \nPlease use a valid URL for the content URL", preferredStyle: UIAlertControllerStyle.alert)
+                
+                self.present(alert, animated: true, completion: nil)
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: { action in
+                    
+                    switch action.style {
+                        case .default:
+                            print("default")
+
+                            self.textView.text = ""
+                            self.player = nil
+                            self.dismiss(animated: true, completion: nil)
+                        
+                        case .cancel:
+                            print("cancel")
+                       
+                        
+                        case .destructive:
+                            print("destructive")
+                    
+                    
+                    }
+                }))
+
+                return
+            }
+
+            let n = NSKeyValueObservingOptions()
+            let asset = AVURLAsset(url: contentURL)
+            playerItem = AVPlayerItem(asset: asset)
+
+            playerItem!.addObserver(self, forKeyPath: "timedMetadata", options: n, context: nil)
+            player = AVPlayer(playerItem: playerItem)
+
+            // Create a player layer for the player.
+            playerLayer = AVPlayerLayer(player: player)
+            playerLayer?.frame = videoView.layer.bounds
+            videoView.layer.addSublayer(playerLayer!)
+            player?.play()
+        }
+    }
+}
+
--- AVBasicPlayback/ViewController.swift
+++ AVBasicPlayback/ViewController.swift
@@ -11,66 +11,43 @@ import AVKit
 import AVFoundation
 
 class ViewController: UIViewController {
+    
+    //@IBOutlets
+    //=====================================================
+    @IBOutlet weak var textField: UITextField!
+
+    //@Objects
+    //=====================================================
     var Player: AVPlayer!
     var PlayerItem: AVPlayerItem!
+    var URL: String! = ""
     
+    //@IBActions
+    //=====================================================
     @IBAction func play(_ sender: Any) {
-   
-        // let url = URL(string: "http://test.hls.liveos.micdn.skycdn.it/ethan367/index.m3u8")
-        let url = URL(string: "http://test.hls.liveos.micdn.skycdn.it/11519/pocv7/index.m3u8")
-       // PlayerItem = AVPlayerItem(url: url!)
-     
-        let asset=AVURLAsset(url: url!)
-        let PlayerItem=AVPlayerItem(asset: asset)
-
-        //let player=AVPlayer(playerItem: item)
-       
-
-        let n = NSKeyValueObservingOptions()
-        PlayerItem.addObserver(self, forKeyPath: "timedMetadata", options: n, context: nil)
-        Player = AVPlayer(playerItem: PlayerItem)
-        let controller = AVPlayerViewController()
-                controller.player = Player
-        
-        
-                // Modally present the player and call the player's play() method when complete.
-                present(controller, animated: true) {
-                    self.Player.play()
-                }
+        self.URL = textField.text
+        Shared.shared.stringValue = self.URL
+        self.dismiss(animated: true, completion: nil)
+        self.performSegue(withIdentifier: "passToView", sender: self)
     }
     
-    
-    
-    override func observeValue(forKeyPath: String?, of: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-        if forKeyPath != "timedMetadata" { return }
-        
-        let data: AVPlayerItem = of as! AVPlayerItem
-        for item in data.timedMetadata! {
-            print(item.value!)
-        }
-        
-        
-  
-        if forKeyPath != "error" { return }
-        let data_: AVPlayerItem = of as! AVPlayerItem
-        for item in data_.timedMetadata! {
-            print(item.value!)
-        }
-        
-//        for item in PlayerItem.error{
-//            print(item.value)
-//        }
-        
-        for item in data.timedMetadata! {
-            print(item.value!)
-        }
+    //@Override functions
+    //=====================================================
+    override func viewDidLoad() {
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        textField.becomeFirstResponder()
+        textField.selectedTextRange = textField.textRange(from: textField.beginningOfDocument, to: textField.endOfDocument)
     }
-
+    
 
 }
 
+//@ Other data structures
+//=====================================================
+final class Shared {
+    static let shared = Shared()
+    
+    var stringValue : String!
+}
