06fffed041c7852cab901c1d9a2a5e0e05dd5737
Matteo Zardoni <matteo@MacBook-Pro-di-Matteo-Zardoni-Live-Reply.local>
2017-12-18 09:14:09 +0100

    Initial commit

--- AVBasicPlayback/AppDelegate.swift
+++ AVBasicPlayback/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,8 +16,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSessionCategoryPlayback)
+        }
+        catch {
+            print("Setting category to AVAudioSessionCategoryPlayback failed.")
+        }
+        
         return true
+        // Override point for customization after application launch.
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
--- AVBasicPlayback/ViewController.swift
+++ AVBasicPlayback/ViewController.swift
@@ -7,14 +7,65 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    var Player: AVPlayer!
+    var PlayerItem: AVPlayerItem!
+    
+    @IBAction func play(_ sender: Any) {
+   
+        // let url = URL(string: "http://test.hls.liveos.micdn.skycdn.it/ethan367/index.m3u8")
+        let url = URL(string: "http://test.hls.liveos.micdn.skycdn.it/11519/pocv7/index.m3u8")
+       // PlayerItem = AVPlayerItem(url: url!)
+     
+        let asset=AVURLAsset(url: url!)
+        let PlayerItem=AVPlayerItem(asset: asset)
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //let player=AVPlayer(playerItem: item)
+       
+
+        let n = NSKeyValueObservingOptions()
+        PlayerItem.addObserver(self, forKeyPath: "timedMetadata", options: n, context: nil)
+        Player = AVPlayer(playerItem: PlayerItem)
+        let controller = AVPlayerViewController()
+                controller.player = Player
+        
+        
+                // Modally present the player and call the player's play() method when complete.
+                present(controller, animated: true) {
+                    self.Player.play()
+                }
     }
-
+    
+    
+    
+    override func observeValue(forKeyPath: String?, of: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if forKeyPath != "timedMetadata" { return }
+        
+        let data: AVPlayerItem = of as! AVPlayerItem
+        for item in data.timedMetadata! {
+            print(item.value!)
+        }
+        
+        
+  
+        if forKeyPath != "error" { return }
+        let data_: AVPlayerItem = of as! AVPlayerItem
+        for item in data_.timedMetadata! {
+            print(item.value!)
+        }
+        
+//        for item in PlayerItem.error{
+//            print(item.value)
+//        }
+        
+        for item in data.timedMetadata! {
+            print(item.value!)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
